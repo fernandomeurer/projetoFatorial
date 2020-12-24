4a349141cd3aa301335ba2417c39e84112e34c11
Fernando Meurer <curso@devmedia>
2020-12-24 10:56:26 -0300

    Criacao da primeira versao da classe fatorial

---
+++ Fatorial.java
@@ -0,0 +1,15 @@
+public class Fatorial {
+    public static void main(String[] args) {
+        System.out.println(CalcularFatorial(5));
+    }
+
+    public static int CalcularFatorial(int n) {
+        int resultado = 1;
+
+        for (int i = 2; i <= n; i++) {
+            resultado = resultado * i;
+        }
+
+        return resultado;
+    }
+}
