ac0ce568a3961c4ad1f3eb5f505471812331e7ab
Fernando Meurer <curso@devmedia>
2021-01-11 08:46:52 -0300

    Alteracao para o calculo correto do fatorial de zero

--- Fatorial.java
+++ Fatorial.java
@@ -5,10 +5,13 @@ public class Fatorial {
             throw new IllegalArgumentException("Não existe fatorial para números negativos");
         int resultado = 1;
 
-        for (int i = 2; i <= n; i++) {
-            resultado = resultado * i;
-        }
+        if (n != 0) {
 
+            for (int i = 2; i <= n; i++) {
+                resultado = resultado * i;
+            }
+        }
+        
         return resultado;
     }
 }
